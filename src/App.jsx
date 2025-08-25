c8c5f2bd50e27a853393acef801f7ba815177b03
Salman Zulfiqar Shaikh <salmanzulfiqar04@gmail.com>
2025-08-26 04:15:47 +0500

    added generate Password functionallity through callback(function and dependencies) if num allowed it will aslso gen num and same dfor strings

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useCallback } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -7,7 +7,22 @@ function App() {
 
   const [length, lengthSetter] = useState(8);
   const [numberAllowed, setNumberAllowed] = useState(true);
- const [symbolAllowed, setSymbolAllowed] = useState(true);
+ const [symbolAllowed, setSymbolAllowed] = useState(false);
+ const [password, setPassword] = useState('');
+
+ const generatePassword = useCallback(()=>{
+  let pass = "";
+  let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+  if(numberAllowed){ str += "0123456789";} 
+  if(symbolAllowed){str += "!@#$%^&*()_+";}
+
+  for(i = 1; i<=length; i++){
+    let char = Math.floor(Math.random()*str.length + 1);
+  }
+ }, [length, numberAllowed, symbolAllowed,setPassword])
+
+ 
 
   return (
     <>
