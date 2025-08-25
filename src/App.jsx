03a41907201cdff6278bd0527726a8ff38ce8e1c
Salman Zulfiqar Shaikh <salmanzulfiqar04@gmail.com>
2025-08-26 04:36:21 +0500

    designed some basic ui

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,8 @@ function App() {
 
   for(i = 1; i<=length; i++){
     let char = Math.floor(Math.random()*str.length + 1);
+    pass += str.charAt(char);
+    setPassword(pass);
   }
  }, [length, numberAllowed, symbolAllowed,setPassword])
 
@@ -26,7 +28,12 @@ function App() {
 
   return (
     <>
-            <h1 className='text-white text-4xl text-center'> Password Generator </h1>
+            <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my8 text-orange-400 bg-gray-700'>
+              <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+                <input type="text" className='w-full px-3 py-2 text-yellow-300' placeholder='Password' readOnly value={password} />
+                <button className='bg-orange-400 text-yellow-200 px-3 py-2 hover:bg-orange-500' onClick={generatePassword}>Copy</button>
+              </div>
+            </div>
           
     </>
   )
